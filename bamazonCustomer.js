76d24f25153ea557a7de947c1122ba3d4c558f53
Bryce Stockman <brycestockman@gmail.com>
2017-08-04 22:54:49 -0400

    working app and updated readme

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,57 +23,75 @@ connection.connect(function(err) {
 var displayItems = function() {
   connection.query('SELECT * FROM products', function(err, res) {
       for (var i = 0; i < res.length; i++) {
-        console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Price: " + "$" + res[i].price);
+        console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Price: " + "$" + res[i].price + " | " + "Stock Quantity: " + res[i].stock_quantity);
       }
       console.log(" ");
-      buyItems();
+      promptCustomer(res);
   })
 };
 
-var buyItems = function() {
-  inquirer.prompt([
-    {
-      name: "ID",
-      type: "input",
-      message: "Type the ID of the product you would like to buy: ",
-      validate: function(value) {
-        if (isNaN(value) == false) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    {
-      name: "units",
-      type: "input",
-      message: "How many units of this product would you like to buy?",
-      validate: function(value) {
-        if (isNaN(value) == false) {
-          return true;
-        } else {
-          return false;
-        }
+// INQUIRER PROMPTS
+
+var buyProductsPrompt = {
+  type: 'input',
+  message: 'Type the ID of the product you would like to buy: ',
+  name: 'buy_products'
+};
+
+var quantityPrompt = {
+  type: 'input',
+  message: 'How many units of this product would you like to buy?',
+  name: 'product_quantity'
+};
+
+var restartPrompt = {
+  type: 'list',
+  message: 'Would you like to buy another item?',
+  choices: ["Yes", "No"],
+  name: 'restart_prompt'
+};
+
+var promptCustomer = function(res) {
+  inquirer.prompt([buyProductsPrompt]).then(function(inquirerResponse) {
+    var chosenProductID = parseInt(inquirerResponse.buy_products);
+    for (var i = 0; i < res.length; i++) {
+      if(res[i].id === chosenProductID) {
+        var id = i;
+        inquirer.prompt([quantityPrompt]).then(function(inquirerResponse) {
+          var chosenQuantity = parseInt(inquirerResponse.product_quantity);
+          if((res[id].stock_quantity - chosenQuantity) >= 0) {
+            var newQuantity = res[id].stock_quantity - chosenQuantity;
+            var totalCost = res[id].price * chosenQuantity;
+            var sql = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+            var values = ['products', 'stock_quantity', newQuantity, 'id', chosenProductID];
+            connection.query(sql, values, function(err, res) {
+              if(err){
+                console.log(err);
+                connection.end();
+              }
+
+              // ALERT USER ON PRODUCTS BOUGT AND TOTAL COST OF TRANSACTION
+
+              console.log("Product(s) bought!" + "\n" + "Total Cost of Transaction: $" + totalCost);
+              inquirer.prompt([restartPrompt]).then(function(inquirerResponse) {
+                if (inquirerResponse.restart_prompt === "Yes") {
+                  displayItems();
+                } else{
+                  console.log("Thank You!");
+                  connection.end();
+                }
+              })
+            })
+          }
+
+          // IF THE NUMBER IS GREATER THAN THE PRODUCT'S STOCK_QUANTITY AMOUNT, ALERT THE USER AND RESTART THE PROMPTCUSTOMER FUNCTION
+
+          else {
+            console.log("Insufficient Quantity! Please enter a number less than or equal to the selected item's available quantity.");
+            promptCustomer(res);
+          }
+        })
       }
     }
-  ]).then(function(answer) {
-    console.log(" ");
-    checkQuantity();
   })
-};
-//
-var checkQuantity = function() {
-  console.log("Checking stock...");
-// //   var query = 'SELECT stock_quantity, price FROM products WHERE id =?';
-// //   var params = answer.id;
-// //     connection.query(query, params, function(err, res) {
-// //       if (res[0].stock_quantity < answer.units) {
-// //         console.log("Sorry, we are out of stock!");
-// //         buyItems();
-// //       } else {
-// //         var totalPrice = answer.units * res[0].price;
-// //         var newQuantity = res[0].stock_quantity - answer.quantity;
-// //         console.log("Total: $" + totalPrice);
-// //       }
-// //     })
 }
